c1fef789ec425d0b1ccd789b2bcaa8f1443c13b9
Hadiii <h.mousavifakhr@gmail.com>
2022-09-26 22:59:19 +0330

    Fisaghoores Project

---
+++ Fisaghoores/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fisaghoores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter Side A: ");
+            var sideA = Convert.ToDouble(Console.ReadLine());
+
+            System.Console.WriteLine("Enter Side B: ");
+            var sideB = Convert.ToDouble(Console.ReadLine());
+
+            var result = (sideA * sideA) + (sideB * sideB);
+
+            var sideC = Math.Sqrt(result);
+
+            System.Console.WriteLine($"side C is {sideC}");
+        }
+    }
+}
