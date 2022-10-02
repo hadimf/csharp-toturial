49e0cf6d23f33bce2e05e5e9feef11e6f62df7a9
Hadiii <h.mousavifakhr@gmail.com>
2022-10-02 14:29:37 +0330

    Television Project

---
+++ Television/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Television
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = 0;
+            bool watching = false;
+
+            do
+            {
+                Console.Clear();
+                System.Console.WriteLine($"channel: {++channel}");
+                System.Console.WriteLine("do u want to watching? (y/n)");
+                var answer = Console.ReadLine().ToLower();
+                watching = answer == "n" ? true : false;
+
+            } while (watching);
+
+            System.Console.WriteLine($"Thanks For Watching channel {channel} ...");
+        }
+    }
+}
